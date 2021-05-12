a9baeeee747a76f70bd97d291b63f85df0b9c2d2
James Hanna <jameshanna@jamess-mbp-2.lan>
2021-05-12 13:50:49 +1000

    Created Codewarrior file to integrate modules

---
+++ finalProject/Sources/functions.h
@@ -0,0 +1,11 @@
+#ifndef    FUNCTIONS_HEADER
+#define    FUNCTIONS_HEADER
+
+//Initialise module functions here
+
+
+
+
+
+
+#endif
---
+++ finalProject/Sources/structs.h
@@ -0,0 +1,11 @@
+#ifndef    STRUCTS_HEADER
+#define    STRUCTS_HEADER
+
+//Initialise structs and global variables here
+
+
+
+
+
+
+#endif
