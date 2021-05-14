5ea92cf43b51734c15d4fed9a6b5f5e27b7807bc
KNN-6948 <1007426372@qq.com>
2021-05-14 23:55:46 +0800

    demo1 map

---
+++ gridmap/demo1.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import seaborn as sb
+import math
+import random
+
+plt.ion()  #打开交互模式
+while 1:
+    # Read data
+    # np.random.seed(20210511)
+    np.random.seed(random.randint(20000000,30000000))
+
+    x = np.random.randn(8, 8)*40
+    print(x)
+
+    # Format data
+    # Create fig
+    # fig, i = plt.subplots(figsize=(6,6))
+    # print(fig)
+
+    # Fill the fig
+    cmap = "greys"
+    sb.heatmap(x, vmin=0,#图例（右侧颜色条color bar）中最小显示值
+                 vmax=40, cmap=plt.get_cmap('Greys')) #annot=True
+    plt.show(block=False)
+    plt.pause(0.1)
+    plt.cla()  # Clear axis
+    plt.clf()  # Clear figure
+    # plt.close(fig)
+
+    # break
+    # Plot
