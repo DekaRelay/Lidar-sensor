4612b34421eba5107a8db4abaf9cc5bd524d9bd1
James Hanna <jameshanna@jamess-mbp-2.lan>
2021-05-19 16:19:57 +1000

    Debugging servo code

--- rotateLiDAR/Sources/main.c
+++ rotateLiDAR/Sources/main.c
@@ -8,7 +8,8 @@ void verticalShift(double);                   //Controls vertical movement
 void initLidar(void);                         //Initialises LiDAR communication
 double getDistance(void);                     //Obtains the distance info from LiDAR
 __interrupt void tovisr(void);                //Interrupt to manage timer count overflows
-void  resetLiDAR(void); 
+void  resetLiDAR(void);
+void servosInit(void);
 
 //Initialisation of variables
 int edge1;                                    //Stores the timer count at first edge
@@ -60,12 +61,13 @@ void main(void) {
 
 
    */
+  servosInit();
   initLidar();
   //while (1){
-    horizontalShift(90); 
-    verticalShift(90); 
-    distance = getDistance();
-    //resetLiDAR(); 
+    horizontalShift(120);
+    //verticalShift(90);
+  //  distance = getDistance();
+    //resetLiDAR();
  // }
 
 
@@ -90,31 +92,16 @@ void verticalShift(double vertical){
 
 
   //Conversion of degrees to ON period
-  double DutyCycle   = ((vertical/180)*(1.2) + 0.9)/20;
+  double DutyCycle = ((vertical/180)*(1.2) + 0.9)/20;
   double ON = DutyCycle * 30000;
-
-
-  PWMCLK = 0;                   //  Clock A is selected as the clock source
-  PWMPOL = 32;                  //  Initial Output is set to HIGH
-  PWMPRCLK = 4;                 //  Prescaler of 16 selected
-  PWMCTL = 0x4C;                //  Concatenate PWM4:PWM5
-  PWMCAE = 0;                   //  Left Align selected
-
 
   PWMPER45 = 30000;              //  Period is set to 30,000
   PWMDTY45 = ON;                 //  Duty set according to input angle
 
   PWME_PWME5 = 1;                //  PWM5 is enabled
 
-
-  //Wait for 10 PWM cycles before disabling signal
-  while (PWMCNT5 <= 100);
-
- //PWME = 0;                     //  Disable PWM Cycle
- //PWMCNT5 = 0;                  //  Reset the count
-
+  while (PWMCNT5 <= 100);        // Set delay for servo to move
 }
-
 
 //Function to enable movement in horizontal axis
 void horizontalShift(double horizontal){
@@ -127,15 +114,15 @@ void horizontalShift(double horizontal){
 
 
   //Conversion from degrees to ON period
-  double DutyCycle   = ((horizontal/180)*(1.2) + 0.9)/20;
+  double DutyCycle = ((horizontal/180)*(1.2) + 0.9)/20;
   double ON = DutyCycle * 30000;
 
   //Initialisation of PWM Signal
-  PWMCLK = 0;                   //  Clock B is selected as the clock source
+ /* PWMCLK = 0x00;                //  Clock B is selected as the clock source
   PWMPOL = 128;                 //  Initial Output is set to HIGH
   PWMPRCLK = 4;                 //  Prescaler of 16 selected
   PWMCTL = 0x8C;                //  Concatenate PWM6:PWM7
-  PWMCAE = 0;                   //  Left Align selected
+  PWMCAE = 0;                   //  Left Align selected   */
 
 
   PWMPER67 = 30000;             //  Period is set to 30,000
@@ -143,12 +130,7 @@ void horizontalShift(double horizontal){
 
   PWME_PWME7 = 1 ;              //  PWM7 is enabled
 
-
- //Disabling of PWM Signal after motor has moved
-while (PWMCNT7 <= 100);         //  Run PWM for 10 Cycles to shift servo
-//PWME = 0;                      //  Disable PWM Cycle
-//PWMCNT7 = 0;                   //  Reset the count
-
+  while (PWMCNT7 <= 100);         //  Run PWM for 10 Cycles to shift servo
 }
 
 
@@ -166,29 +148,21 @@ void initLidar(void){
 double getDistance(void){
 
  PTH_PTH0 = 0;                  //  Send trigger signal to LiDAR
-
  TSCR1 = 0x90;                  //  Enable timer count and fast timer flag clear
  TSCR2 = 0x04;                  //  Set prescaler to 16
-
  TIOS_IOS1 = 0;                 //  Enable input capture
  DLYCT = 0x02;                  //  Set delay count to 1024 E cycles to eliminate noise
  ICOVW_NOVW1 = 1;               //  Disable overwrite to TCO register
  ICSYS = 0;                     //  Disable queue mode
-
  TCTL4 = 0x04;                  //  Capture the rising edge
  TFLG1_C1F = 1;                 //  Clear C1F flag
 
  while (!(TFLG1_C1F));          //  Wait for rising edge
 
  TFLG2 = 0x80;                  //  Clear the TOF flag
-
  TSCR2_TOI = 1;                 //  Enable TCNT overflow interrupt
-
  EnableInterrupts;              // Enable interrupts
-
- //TFLG1_C1F = 1;                 //  Clear the C1F flag
  edge1 = TC1;                   //  Store the first edge time in edge1
-
  TCTL4 = 0x08;                  //  Capture the falling edge
 
  while (!(TFLG1_C1F));          //  Wait for falling edge
@@ -196,11 +170,10 @@ double getDistance(void){
  pulse_width = TC1 - edge1;     //  Calculate the pulse width as the difference of the edges
 
  if (TC1 < edge1){              //  Check if the second edge is smaller
-  overflow--;                   //  Decrement the overflow counter
+   overflow--;                   //  Decrement the overflow counter
  }
 
  PTH_PTH0 = 1;                  //  Disable LiDAR trigger
-
  length = (2 * (double)pulse_width)/3000;   // Convert the width from counts to milliseconds
 
  return length;                 // Return the timed width of the pulse
@@ -211,15 +184,23 @@ double getDistance(void){
 #pragma CODE_SEG __NEAR_SEG NON_BANKED
 __interrupt void tovisr(void){
   TFLG2 = 0x80;                  // Clear the timer overflow flag
-  overflow++;                    // Increment the overflow 
+  overflow++;                    // Increment the overflow
 }
 
-//Function to reset LiDAR values after measurement 
+//Function to reset LiDAR values after measurement
 void resetLiDAR(void){
-   overflow = 0; 
-   TC1 = 0; 
-   pulse_width = 0; 
-   length = 0; 
-   edge1 = 0; 
-   
+   overflow = 0;
+   TC1 = 0;
+   pulse_width = 0;
+   length = 0;
+   edge1 = 0;
+}
+
+void servosInit(void){
+  DDRP = 0x00;                     // Set direction of PORTP to output
+  PWMCLK = 0x00;                  //  Clocks A & B are selected as the clock source
+  PWMPOL = 0xA0;                 //  Initial Output is set to HIGH
+  PWMPRCLK = 4;                 //  Prescaler of 16 selected
+  PWMCTL = 0xCC;               //  Concatenate PWM6:PWM7 and PWM4:PWM5
+  PWMCAE = 0;                 //  Left Align selected
 }
